f52ef7dff29c310e872f99b81245887daf0497f8
chowizard <chowizard@hotmail.com>
2017-09-20 13:43:49 +0900

    Prefab test.
    - Internally, reported that prefab data was broken when script code changed in Unity 2017.1.1f1.
    
    - But, my testing is right. Cannot found any problem.
    
    - It is probably cause to fail to compile 3th party plugin software.

---
+++ Program/Client/Assets/Scripts/SceneMain.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneMain : MonoBehaviour
+{
+    public bool flag;
+
+    public int id;
+
+    public string sceneName;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Scene was started.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
