10188c3ad466beb691469a8133c5c8519b8fbfab
chowizard <chowizard@hotmail.com>
2017-09-21 17:10:46 +0900

    Make network player object which is possible to control
    - This is working by local network.

---
+++ Program/Client/Assets/Scripts/PlayerComponentMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerComponentMove : MonoBehaviour
+{
+    public float velocity;
+
+    // Use this for initialization
+    void Start()
+    {
+        transform.position = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float axisX = Input.GetAxis("Horizontal");
+        float axisZ = Input.GetAxis("Vertical");
+
+        float positionX = axisX * velocity * Time.deltaTime;
+        float positionZ = axisZ * velocity * Time.deltaTime;
+
+        transform.Translate(axisX, transform.position.y, axisZ);
+    }
+}
