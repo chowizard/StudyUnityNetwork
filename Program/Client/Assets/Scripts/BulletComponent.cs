65e334515ad9d21e2ed59656e2f8229510d5acc7
chowizard <chowizard@hotmail.com>
2017-09-26 12:13:18 +0900

    Implement shooting player mock-up.

---
+++ Program/Client/Assets/Scripts/BulletComponent.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletComponent : MonoBehaviour
+{
+    public Vector3 velocity;
+    public float speed = 4.0f;
+
+    private Rigidbody selfRigidbody;
+
+    // Use this for initialization
+    private void Start()
+    {
+        selfRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        CheckVelocity();
+    }
+
+    private void CheckVelocity()
+    {
+        Vector3 currentVelocity = velocity * speed;
+
+        if(selfRigidbody.velocity != currentVelocity)
+            selfRigidbody.velocity = currentVelocity;
+
+    }
+}
--- Program/Client/Assets/Scripts/PlayerComponentMove.cs
+++ Program/Client/Assets/Scripts/PlayerComponentMove.cs
@@ -1,19 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class PlayerComponentMove : MonoBehaviour
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerComponentMove : NetworkBehaviour
 {
     public float velocity;
 
+    public GameObject bulletObject;
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
     // Use this for initialization
-    void Start()
+    private void Start()
     {
         transform.position = Vector3.zero;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
+    {
+        if(!isLocalPlayer)
+            return;
+
+        CheckUpdateMove();
+        CheckUpdateFire();
+    }
+
+    private void CheckUpdateMove()
     {
         float axisX = Input.GetAxis("Horizontal");
         float axisZ = Input.GetAxis("Vertical");
@@ -22,5 +40,23 @@ public class PlayerComponentMove : MonoBehaviour
         float positionZ = axisZ * velocity * Time.deltaTime;
 
         transform.Translate(axisX, transform.position.y, axisZ);
+    }
+
+    private void CheckUpdateFire()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+            Fire();
+    }
+
+    private void Fire()
+    {
+        Vector3 spawnPosition = transform.position - transform.forward;
+        GameObject bullet = Instantiate<GameObject>(bulletObject, spawnPosition, Quaternion.identity);
+
+        BulletComponent bulletComponent = bullet.GetComponent<BulletComponent>();
+        bulletComponent.velocity = -transform.forward;
+        bulletComponent.speed = 4.0f;
+
+        Object.Destroy(bullet, 2.0f);
     }
 }
