e443026b48195bd3b36c4ab4bb24e2e43f02a2f8
chowizard <chowizard@hotmail.com>
2017-10-13 17:45:25 +0900

    Add functions that is manage to NetworkClient objects.

--- Program/Client/Assets/Scripts/NetworkControllerServer.cs
+++ Program/Client/Assets/Scripts/NetworkControllerServer.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 using UnityEngine;
 using UnityEngine.Networking;
@@ -7,7 +8,7 @@ using UnityEngine.Networking;
 public sealed class NetworkControllerServer
 {
     private NetworkManager networkManager;
-    private Dictionary<int, NetworkClient> networkClients;
+    private Dictionary<int, NetworkClient> networkClients = new Dictionary<int, NetworkClient>();
 
     public NetworkControllerServer(NetworkManager networkManager)
     {
@@ -16,6 +17,8 @@ public sealed class NetworkControllerServer
 
     public void Setup()
     {
+        NetworkServer.Reset();
+
         NetworkServer.RegisterHandler(MsgType.Error, OnError);
         NetworkServer.RegisterHandler(MsgType.Connect, OnConnected);
         NetworkServer.RegisterHandler(MsgType.Disconnect, OnDisconnected);
@@ -27,6 +30,12 @@ public sealed class NetworkControllerServer
     {
         NetworkServer.DisconnectAll();
         NetworkServer.Reset();
+    }
+
+    public NetworkClient GetNetworkClient(int connectionId)
+    {
+        NetworkClient data;
+        return networkClients.TryGetValue(connectionId, out data) ? data : null;
     }
 
     public void OnError(NetworkMessage networkMessage)
@@ -56,5 +65,63 @@ public sealed class NetworkControllerServer
         Debug.Log(message);
 
         networkManager.message = message;
+    }
+
+    public NetworkClient[] NetworkClients
+    {
+        get
+        {
+            return (networkClients.Count > 0) ? networkClients.Values.ToArray() : null;
+        }
+    }
+
+    public int NetworkClientCount
+    {
+        get
+        {
+            return networkClients.Count;
+        }
+    }
+
+    private NetworkClient AddNetworkClient(NetworkConnection connection)
+    {
+        if(connection == null)
+            return null;
+
+        NetworkClient networkClient = new NetworkClient(connection);
+
+        networkClients.Add(connection.connectionId, networkClient);
+
+        return networkClient;
+    }
+
+    private bool RemoveNetworkClient(int connectionId)
+    {
+        return networkClients.Remove(connectionId);
+    }
+
+    private bool RemoveNetworkClient(NetworkClient networkClient)
+    {
+        if(networkClient == null)
+            return false;
+
+        if(networkClient.connection != null)
+        {
+            return RemoveNetworkClient(networkClient.connection.connectionId);
+        }
+        else
+        {
+            foreach(var pair in networkClients)
+            {
+                NetworkClient currentNetClient = pair.Value;
+                if(currentNetClient == null)
+                    continue;
+
+                if(currentNetClient == networkClient)
+                    return networkClients.Remove(pair.Key);
+            }
+
+            return false;
+        }
     }
 }
