3c0c8909b044fc8537d4adf233c1701568921db4
chowizard <chowizard@hotmail.com>
2017-10-31 11:59:12 +0900

    - Only clients can to input control command for player object's moving.

--- Program/Client/Assets/Scripts/Character/CharacterComponentInputControl.cs
+++ Program/Client/Assets/Scripts/Character/CharacterComponentInputControl.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class CharacterComponentInputControl : CharacterComponent
 {
@@ -15,10 +16,27 @@ public class CharacterComponentInputControl : CharacterComponent
     // Update is called once per frame
     protected override void Update()
     {
+        NetworkIdentity networkIdentity = GetComponent<NetworkIdentity>();
+        if(networkIdentity == null)
+            return;
+
+        if(networkIdentity.isServer == true)
+            UpdateServer();
+
+        if(networkIdentity.isClient == true)
+            UpdateClient();
+    }
+
+    private void UpdateServer()
+    {
+    }
+
+    private void UpdateClient()
+    {
         float axisX = Input.GetAxis("Horizontal");
         float axisZ = Input.GetAxis("Vertical");
 
-       if((axisX != 0.0f) || (axisZ != 0.0f))
+        if((axisX != 0.0f) || (axisZ != 0.0f))
         {
             CharacterComponentMove moveComponent = owner.GetCharacterComponent<CharacterComponentMove>();
             if(moveComponent != null)
