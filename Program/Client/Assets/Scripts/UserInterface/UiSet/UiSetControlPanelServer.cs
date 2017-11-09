d221e221213b80ef97c2fe5cac0ab89f9949c059
chowizard <chowizard@hotmail.com>
2017-11-09 17:53:13 +0900

    NetworkTransform's properties in Unity editor inspector don't fix through script code. : They are not provide public interface. That is only can do control in inspector.

--- Program/Client/Assets/Scripts/UserInterface/UiSet/UiSetControlPanelServer.cs
+++ Program/Client/Assets/Scripts/UserInterface/UiSet/UiSetControlPanelServer.cs
@@ -2,18 +2,37 @@
 using System.Collections.Generic;
 
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class UiSetControlPanelServer : UiSet
 {
     public Button uiButtonTerminate;
+
+
     public Button uiButtonAddNpc;
     public Slider uiSliderAddNpcUnitCount;
     public Text uiTextAddNpcUnitCount;
-
     public int addNpcUnitMinimum = 1;
     public int addNpcUnitMaximum = 10;
     private int currentAddNpcUnitCount;
+
+
+    public Slider uiSliderNpcSendRate;
+    public Text uiTextNpcSendRate;
+    private int npcSendRate;
+
+    public Slider uiSliderNpcMovementThreshold;
+    public Text uiTextNpcMovementThreshold;
+    private float npcMovementThreshold;
+
+    public Slider uiSliderNpcSnapThreshold;
+    public Text uiTextNpcSnapThreshold;
+    private float npcSnapThreshold;
+
+    public Slider uiSliderNpcInterpolateMovementFactor;
+    public Text uiTextNpcInterpolateMovementFactor;
+    private float npcInterpolateMovementFactor;
 
     public void OnClickTerminate()
     {
@@ -45,14 +64,13 @@ public class UiSetControlPanelServer : UiSet
         Debug.Assert(uiButtonAddNpc != null);
         Debug.Assert(uiSliderAddNpcUnitCount != null);
         Debug.Assert(uiTextAddNpcUnitCount != null);
-
-        uiTextAddNpcUnitCount.text = string.Format("Add NPC Unit Count : {0}", currentAddNpcUnitCount);
     }
 
     // Update is called once per frame
     private void Update()
     {
         UpdateAddNpcUnitCount();
+        UpdateNetworkConfiguration();
     }
 
     private void UpdateAddNpcUnitCount()
@@ -67,6 +85,70 @@ public class UiSetControlPanelServer : UiSet
         {
             currentAddNpcUnitCount = (int)uiSliderAddNpcUnitCount.value;
             uiTextAddNpcUnitCount.text = string.Format("Add NPC Unit Count : {0}", currentAddNpcUnitCount);
+        }
+    }
+
+    private void UpdateNetworkConfiguration()
+    {
+        UpdateNpcSendRate();
+        UpdateNpcMovementThreshold();
+        UpdateNpcSnapThreshold();
+        UpdateNpcInterpolateMovementFactor();
+    }
+
+    private void UpdateNpcSendRate()
+    {
+        if(uiSliderNpcSendRate.value != npcSendRate)
+        {
+            npcSendRate = (int)uiSliderNpcSendRate.value;
+
+            //CharacterEntity[] entities = EntityManager.Instance.Entities;
+            //if(entities != null)
+            //{
+            //    foreach(CharacterEntity characterEntity in entities)
+            //    {
+            //        if(characterEntity == null)
+            //            continue;
+
+            //        if(characterEntity.property.isPlayer == true)
+            //            continue;
+
+            //        NetworkTransform networkTransform = characterEntity.GetComponent<NetworkTransform>();
+            //        if(networkTransform == null)
+            //            continue;
+
+            //        //networkTransform.
+            //    }
+            //}
+
+            uiTextNpcSendRate.text = string.Format("NPC Network Send Rate : {0} per seconds.", npcSendRate);
+        }
+    }
+
+    private void UpdateNpcMovementThreshold()
+    {
+        if(uiSliderNpcMovementThreshold.value != npcMovementThreshold)
+        {
+            npcMovementThreshold = uiSliderNpcMovementThreshold.value;
+            uiTextNpcMovementThreshold.text = string.Format("NPC Network Movement Threshold : {0}", npcMovementThreshold);
+        }
+    }
+
+    private void UpdateNpcSnapThreshold()
+    {
+        if(uiSliderNpcSnapThreshold.value != npcSnapThreshold)
+        {
+            npcSnapThreshold = uiSliderNpcSnapThreshold.value;
+            uiTextNpcSnapThreshold.text = string.Format("NPC Network Snap Threshold : {0}", npcSnapThreshold);
+        }
+    }
+
+    private void UpdateNpcInterpolateMovementFactor()
+    {
+        if(uiSliderNpcInterpolateMovementFactor.value != npcInterpolateMovementFactor)
+        {
+            npcInterpolateMovementFactor = uiSliderNpcInterpolateMovementFactor.value;
+            uiTextNpcInterpolateMovementFactor.text = string.Format("NPC Network Interpolate Movement Factor : {0}", npcInterpolateMovementFactor);
         }
     }
 }
