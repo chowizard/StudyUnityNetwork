c853090bdbc6af20af1de3d6220567d6cce2e7b3
chowizard <chowizard@hotmail.com>
2017-11-09 19:22:29 +0900

    Added NetworkTransformSynchronizer class. : This is for to alternate UnityEngine.Networking.NetworkTransform class

---
+++ Program/Client/Assets/Scripts/Network/NetworkTransformSynchronizer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkTransformSynchronizer : NetworkBehaviour
+{
+    public int sendRate;
+    public float positionThreshold;
+    public float positionInterpolationFactor;
+    public float rotationThresholdEuler;
+    public float rotationInterpolationFactor;
+
+    [SyncVar]
+    private Vector3 position;
+
+    [SyncVar]
+    private Quaternion rotation;
+
+
+    // Use this for initialization
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        UpdateInterpolatePosition();
+        UpdateInterpolateRotation();
+    }
+
+    private void FixedUpdate()
+    {
+        if(sendRate <= 0)
+            return;
+
+
+    }
+
+    private void UpdateInterpolatePosition()
+    {
+    }
+
+    private void UpdateInterpolateRotation()
+    {
+    }
+}
