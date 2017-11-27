27ccddfb68b10484c3080386d822a8133be4ddd6
chowizard <chowizard@hotmail.com>
2017-11-27 10:58:32 +0900

    - Added CharacterComponentCombat class.
    - Updated .gitignore file.
    : It is about to ignore .vscode files.

---
+++ Program/Client/Assets/Scripts/Character/CharacterComponentCombat.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using Chowizard.UnityNetwork.Client;
+
+namespace Chowizard.UnityNetwork.Client.Character
+{
+	[DisallowMultipleComponent]
+	public class CharacterComponentCombat : CharacterComponent
+	{
+		// Use this for initialization
+        protected override void Start()
+        {
+            base.Start();
+
+            owner.AddCharacterComponent(this);
+        }
+
+        // Update is called once per frame
+        protected override void Update()
+        {
+            base.Update();
+        }
+	}
+}
