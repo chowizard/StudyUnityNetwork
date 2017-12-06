fef3425fe11ca5246ca1a5303e2c22f35878cb49
chowizard <chowizard@hotmail.com>
2017-12-06 20:24:35 +0900

    Added HUD UI for to display character entities information.

---
+++ Program/Client/Assets/Scripts/UserInterface/UiHeadupDisplay/UiHudCharacterInformations.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+using Chowizard.UnityNetwork.Client.Core;
+using Chowizard.UnityNetwork.Client.Scene;
+
+namespace Chowizard.UnityNetwork.Client.Ui
+{
+    public class UiHudCharacterInformations : MonoBehaviour
+    {
+        public Text uiTextCharacterId;
+        public Text uiTextDescription;
+
+        private uint characterId;
+        private Vector3 characterPosition;
+
+        // Use this for initialization
+        private void Start()
+        {
+            Debug.Assert(uiTextCharacterId != null);
+            Debug.Assert(uiTextDescription != null);
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            UpdateCheckCharacterId();
+            UpdateCheckDescription();
+        }
+
+        private void UpdateCheckCharacterId()
+        {
+        }
+
+        private void UpdateCheckDescription()
+        {
+        }
+    }
+}
