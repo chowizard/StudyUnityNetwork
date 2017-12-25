25782ac23759d7479088b69bff30c229bd8e4ddc
JoSoowoon <chowizard@hotmail.com>
2017-12-26 00:02:58 +0900

    Implemented to purpose received networking events.

--- Program/Client/Assets/Scripts/Network/Message/NetworkEventClientToServerCharacterMoveTo.cs
+++ Program/Client/Assets/Scripts/Network/Message/NetworkEventClientToServerCharacterMoveTo.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
+using Chowizard.UnityNetwork.Client.Core;
+using Chowizard.UnityNetwork.Client.Character;
+using Chowizard.UnityNetwork.Client.Character.Ai;
+
 namespace Chowizard.UnityNetwork.Client.Network.Message
 {
     public sealed class NetworkEventClientToServerCharacterMoveTo : 
@@ -13,6 +17,27 @@ namespace Chowizard.UnityNetwork.Client.Network.Message
         {
             if(networkMessage == null)
                 return;
+
+            NetworkMessageCharacterMoveTo detailMessage = networkMessage.ReadMessage<NetworkMessageCharacterMoveTo>();
+            if(detailMessage == null)
+                return;
+
+            CharacterEntity character = EntityManager.Instance.GetEntity(detailMessage.characterId);
+            if(character == null)
+                return;
+            
+            CharacterComponentAi aiComponent = character.GetCharacterComponent<CharacterComponentAi>();
+            Vector3 toDirection = Vector3.Normalize(detailMessage.position - character.transform.position);
+            if(toDirection == Vector3.zero)
+                return;
+            
+            Quaternion toRotation = Quaternion.FromToRotation(character.transform.forward, toDirection);
+            Quaternion nowRotation = character.transform.rotation * toRotation;
+
+            CharacterAiConditionNormal aiCondition = new CharacterAiConditionNormal(character);
+            CharacterAiBehaviourMoveToPosition aiBehaviour = new CharacterAiBehaviourMoveToPosition(character, detailMessage.position, nowRotation);
+
+            aiComponent.ChangeAiState(CharacterAiState.eType.Move, aiCondition, aiBehaviour);
         }
 
         public override short MessageCode
